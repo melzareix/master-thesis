8537c592c671714b4e9664ca7d9d397a9bfaf000
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2022-06-13 08:32:02 +0200

    implementation check

--- code/utf32.cpp
+++ code/utf32.cpp
@@ -1,20 +1,17 @@
 inline unsigned nextCodepoint(const char* str, unsigned& idx) {
   unsigned c = 0;
   char b1 = str[idx];
-  // determine number of utf8 bytes
-  unsigned byteLen = multiByteSequenceLength(b1);
-  // read as utf32 string
-  c = readMultiByte(str, b1, idx, byteLen);
+  unsigned byteLen = getLength(b1); // get number of utf-8 bytes
+  c = readMultiByte(str, idx, byteLen); // read full code point
   idx += byteLen;
   return c;
 }
-
 bool traverse(const char * str, unsigned int n) {
   unsigned int idx = 0;
   unsigned int c;
   s0:
     if (idx >= n) return false;
     c = nextCodepoint(str, idx);
-    if (c == 0xA3 /*sterling pound codepoint*/) return true;
+    if (c == 0xA3) return true; /*sterling pound codepoint*/
   return false;
 }
